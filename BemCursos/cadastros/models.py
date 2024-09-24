0f93c7bdca141cda4eee3ec6296341723c1cfb8e
Felipe Caminha <142419397+Fcc2187@users.noreply.github.com>
2024-09-24 11:02:58 -0300

    Update models.py

--- BemCursos/cadastros/models.py
+++ BemCursos/cadastros/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Turma(models.Model):
     nome = models.CharField(max_length=100)
     unidade = models.CharField(max_length=100)
