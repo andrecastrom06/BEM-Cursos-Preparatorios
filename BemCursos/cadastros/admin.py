c8a2d90107c117f4c367a7e8e0c9a76fb3822a7f
andrecastrom06 <alcms@cesar.school>
2024-10-27 20:20:36 -0300

    Models

--- BemCursos/cadastros/admin.py
+++ BemCursos/cadastros/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Turma, Unidade, Aluno
+from .models import Turma, Unidade, Aluno, Nota, Simulado
 
-# Register your models here.
 admin.site.register(Turma)
 admin.site.register(Unidade)
 admin.site.register(Aluno)
+admin.site.register(Nota)
+admin.site.register(Simulado)
