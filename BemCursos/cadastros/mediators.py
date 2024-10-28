157de71d4985d89cc4badbaf69b15415670f8e96
andrecastrom06 <alcms@cesar.school>
2024-10-27 21:13:44 -0300

    Mediator ajeitado

--- BemCursos/cadastros/mediators.py
+++ BemCursos/cadastros/mediators.py
@@ -170,9 +170,3 @@ class RankingMediator:
         )
         rankings = RankingMediator._calcular_medias(rankings, tipo_simulado)
         return rankings.order_by('-media_final', '-media_matematica', '-aluno__idade_em_dias')
-    
-
-class NotaMediator:
-    @staticmethod
-    def obter_notas_aluno(aluno):
-        return Nota.objects.filter(aluno=aluno)
