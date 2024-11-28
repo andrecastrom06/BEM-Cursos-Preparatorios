2cae83ed14d79f0fc89a78b4740b2e7f7a0a4a31
Felipe Caminha <142419397+Fcc2187@users.noreply.github.com>
2024-11-28 11:59:11 -0300

    Update models.py

--- BemCursos/cadastros/models.py
+++ BemCursos/cadastros/models.py
@@ -4,36 +4,20 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 import unicodedata
 
-class Unidade(models.Model):
-    nome = models.CharField(max_length=100, unique=True)
-    def __str__(self):
-        return self.nome
-
-class Turma(models.Model):
-    nome = models.CharField(max_length=100)
-    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE, related_name='turmas')
-
-    def __str__(self):
-        return f'{self.nome} - {self.unidade.nome}'
-    
-#def usada no login para remover possiveis acentos gráficos nos nomes
 def remover_acentos(texto):
-    return ''.join(char for char in unicodedata.normalize('NFD', texto)if unicodedata.category(char) != 'Mn')
+    return ''.join(char for char in unicodedata.normalize('NFD', texto) if unicodedata.category(char) != 'Mn')
 
-
-class Aluno(models.Model):
+class AlunoBase(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     idade_em_dias = models.PositiveIntegerField(editable=False)
-    turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='alunos')
-    is_ver_geral = models.BooleanField(default=False, verbose_name="Permissão para Visualizar Ranking Geral")
 
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
-
+    
     def calcular_idade_em_dias(self):
         hoje = timezone.now().date()
         idade_dias = (hoje - self.data_nascimento).days
@@ -53,28 +37,61 @@ class Aluno(models.Model):
         digito1 = calcular_digito(cpf[:9])
         digito2 = calcular_digito(cpf[:10])
         return cpf[-2:] == f"{digito1}{digito2}"
-
+    
     def gerar_login(self):
-        texto =  f"{self.nome}{self.sobrenome}{self.turma.nome}".lower().strip().replace(' ', '')
+        texto = f"{self.nome}{self.sobrenome}{self.turma.nome}".lower().strip().replace(' ', '')
         return remover_acentos(texto)
 
     def gerar_senha(self):
         return self.cpf
+    
+    class Meta:
+        abstract = True # Classe abstrata
+
+#Herança entre Aluno e Aluno Base
+class Aluno(AlunoBase):
+    turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='alunos')
+    is_ver_geral = models.BooleanField(default=False, verbose_name="Permissão para Visualizar Ranking Geral")
 
     def save(self, *args, **kwargs):
+        if self.data_nascimento.year > 2024:
+            raise ValidationError("Não é permitido adicionar alunos nascidos após 2024.")
+        
         self.calcular_idade_em_dias()
 
-        if not self.user_id:
+        if not self.validar_cpf():
+            raise ValidationError("CPF inválido.")
+
+        if not self.user:
             login = self.gerar_login()
             senha = self.gerar_senha()
 
             if User.objects.filter(username=login).exists():
                 raise ValidationError(f"O nome de usuário '{login}' já existe.")
-
-            user = User.objects.create_user(username=login, password=senha)
-            self.user = user
-
+            
+            try:
+                user = User.objects.create_user(username=login, password=senha)
+                self.user = user
+            except Exception as e:
+                raise ValidationError(f"Erro ao criar o usuário: {str(e)}")
+        
         super().save(*args, **kwargs)
+
+
+class Unidade(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.nome
+
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=100)
+    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE, related_name='turmas')
+
+    def __str__(self):
+        return f'{self.nome} - {self.unidade.nome}'
+    
 
 class Simulado(models.Model):
     TIPOS_SIMULADO = [
@@ -109,6 +126,7 @@ class Simulado(models.Model):
             reverse=True
         )
 
+
 class Nota(models.Model):
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)  
     simulado = models.ForeignKey(Simulado, on_delete=models.CASCADE)  
@@ -117,8 +135,8 @@ class Nota(models.Model):
     
     @property
     def nota_final(self):
-        if self.simulado.tipo == "Colégio Militar": 
-            return (self.acertos_mat + self.acertos_port) / 2  
-        elif self.simulado.tipo == "Escola de Aplicação":
-            return self.acertos_mat + self.acertos_port  
+        if self.simulado.tipo == "CM": 
+            return (self.matematica_acertos + self.portugues_acertos) / 2  
+        elif self.simulado.tipo == "EA":
+            return self.matematica_acertos + self.portugues_acertos  
         return 0
